d64c45c1d1e5d49d5b3236ca00f3cd58bf62e4ff
Ranjithanbu <ranjithjithg@gmail.com>
2024-03-27 00:02:39 -0700

    index modified

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(Express.json())
 const port=process.env.PORT
 connectDB()
 
+app.get('/',(req,res)=>{
+
+ res.status(200).json({message:'site is working'})   
+})
 app.use('/api',MainRouter)
 app.listen(port,()=>{
     console.log("app is listening on the port-",port);
